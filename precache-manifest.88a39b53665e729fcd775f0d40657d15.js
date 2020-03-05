78001351e1a0dda477c2861bf0c79bd5d02b4d7e
AbdelRahman Emam <abdoemamofficial@gmail.com>
2020-03-05 09:03:45 +0200

    Updates

---
+++ precache-manifest.88a39b53665e729fcd775f0d40657d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5526030885b904251afb611d98c37c3",
+    "url": "/MovieSeatBookingApp/index.html"
+  },
+  {
+    "revision": "5b468cb0e6e8ec2a2ad3",
+    "url": "/MovieSeatBookingApp/static/css/main.554a2d7a.chunk.css"
+  },
+  {
+    "revision": "5738dc0038dd1adeaaf3",
+    "url": "/MovieSeatBookingApp/static/js/2.c6ad0ee9.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/MovieSeatBookingApp/static/js/2.c6ad0ee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b468cb0e6e8ec2a2ad3",
+    "url": "/MovieSeatBookingApp/static/js/main.6f887dd2.chunk.js"
+  },
+  {
+    "revision": "f46fd66abcef59f19de5",
+    "url": "/MovieSeatBookingApp/static/js/runtime-main.fbc277d4.js"
+  }
+]);
